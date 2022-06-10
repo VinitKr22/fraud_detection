3d07c9b07eb0b3fc3cf5c3aeddd32bad7c008d7b
VinitKr22 <vinit22914920@gmail.com>
2022-06-10 14:56:00 +0530

    function added

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,10 @@
 print("Adding all the data processing file related to fraud detection")
+a=5
+b=7
+def sum(a,b):
+    c=a+b
+    return c
+def multilication(a,b):
+    d=a*b
+    return d
+print("multiplication function")
